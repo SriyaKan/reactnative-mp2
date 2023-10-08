43451142c5bdd4e2e51a74f1ac63064fbd73b709
Sriya Kantipudi <sriyakantipudi@berkeley.edu>
2023-10-08 01:21:56 -0700

    first commit

--- screens/MovieDetailScreen/MovieDetailScreen.main.jsx
+++ screens/MovieDetailScreen/MovieDetailScreen.main.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { SafeAreaView, Text, Image, ScrollView } from "react-native";
 import { styles } from "./MovieDetailScreen.styles";
 
+//note 
+
 export default function MovieDetailScreen({ route }) {
   // TODO: Recieve the movieItem by destructuring route params.
   return (
