fa242100f41895416b2984c64c29f8c37db2672b
Sriya Kantipudi <sriyakantipudi@berkeley.edu>
2023-10-16 16:23:51 -0700

    finished layout

--- screens/MovieDetailScreen/MovieDetailScreen.main.jsx
+++ screens/MovieDetailScreen/MovieDetailScreen.main.jsx
@@ -4,12 +4,25 @@ import { styles } from "./MovieDetailScreen.styles";
 
 export default function MovieDetailScreen({ route }) {
   // TODO: Recieve the movieItem by destructuring route params.
+  const { movieItem } = route.params;
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView style={styles.container}>
         {/* TODO: Configure this screen to have an image and appropriate text describing the movie. 
                 See the example on the spec for design inspiration.
                 Feel free to use the styles below. */}
+                {/* Image */}
+                <Image 
+                  source = {{uri: movieItem.posterurl}}
+                  style = {{height:100, width:"auto", resizeMode: "contain"}}
+                > 
+                </Image>
+                <Text style={styles.h1}>{movieItem.title}</Text>
+                <Text style={styles.h1}>{movieItem.year}</Text>
+                <Text style={styles.h1}>{movieItem.genres.join(",")}</Text>
+                <Text style={styles.h1}>{movieItem.actors.join(",")}</Text>
+                <Text style={styles.h1}>{movieItem.storyline}</Text>
+
       </ScrollView>
     </SafeAreaView>
   );
